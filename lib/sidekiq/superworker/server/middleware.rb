7ed14036f5c99658158ff85664a6f7ce4701c896
Rahim Jadavji <rahim.jadavji@lendesk.com>
2017-10-03 15:26:28 -0700

    [CORE-1156] Handle specific exceptions raised by the worker to allow us to break out of a superworker chain when required

--- lib/sidekiq/superworker/server/middleware.rb
+++ lib/sidekiq/superworker/server/middleware.rb
@@ -11,6 +11,9 @@ module Sidekiq
         def call(worker, item, queue)
           begin
             return_value = yield
+          rescue Sidekiq::Superworker::Exit => exception
+            @processor.complete_all(item, exception)
+            # This is to catch intentional exits from a SuperWorker
           rescue => exception
             @processor.error(worker, item, queue, exception)
             raise exception
